f0aa933267ff8eae0789ffda431964b26612152b
harishtaskar <harishtaskar001@gmail.com>
2022-07-10 17:40:15 +0530

    10-07-2022 new

--- app/src/main/java/com/redxsolution/Adorable/MainActivity.java
+++ app/src/main/java/com/redxsolution/Adorable/MainActivity.java
@@ -3,9 +3,12 @@ package com.redxsolution.Adorable;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Toast;
 import com.lorentzos.flingswipe.SwipeFlingAdapterView;
 
@@ -17,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<String> al;
     private ArrayAdapter<String> arrayAdapter;
     private int i;
+    Button btnRegister;
 
 
 
@@ -27,6 +31,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnRegister = findViewById(R.id.login_btn);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Login.class);
+                startActivity(intent);
+            }
+        });
 
         al = new ArrayList<>();
         al.add("php");
--- app/src/main/java/com/redxsolution/Adorable/Registration.java
+++ app/src/main/java/com/redxsolution/Adorable/Registration.java
@@ -2,19 +2,14 @@ package com.redxsolution.Adorable;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class Login extends AppCompatActivity {
+public class Registration extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-
+        setContentView(R.layout.activity_registration);
     }
 }
